2a6f173a4caf1540ec407a2b427ed6ad37c48ee7
edabool <elad@neuray.ai>
2022-10-14 07:53:34 -0700

    device mapping info

--- web_app/src/app/entities/radar-device.ts
+++ web_app/src/app/entities/radar-device.ts
@@ -1,17 +1,30 @@
 
 export interface RadarDeviceBrief {
-    name: string;
-    state: string;
-    description: string;
-    device_id: string;
-    enabled: boolean;
+    name: string
+    state: string
+    description: string
+    device_id: string
+    enabled: boolean
 }
 
 export interface RadarDevice {
-    name: string;
-    state: string;
-    description: string;
-    device_id: string;
-    enabled: boolean;
+    name: string
+    state: string
+    description: string
+    device_id: string
+    enabled: boolean
+
+    config: string[]
+
+    device_mapping: DeviceMapping
 }
 
+export interface DeviceMapping {
+    ip: string
+    subnet: string
+    gateway: string
+    device_id: string
+    model: string
+    application: string
+    static_ip: string
+}
