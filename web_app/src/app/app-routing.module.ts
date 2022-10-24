d37b650ef9275e23ac7160c9ce44d041c657994e
edabool <elad@neuray.ai>
2022-10-24 00:19:26 -0700

    remove modules

--- web_app/src/app/app-routing.module.ts
+++ web_app/src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DevicePageComponent } from './radar-device/pages/device-page/device-page.component';
-import { DevicesPageComponent } from './radar-device/pages/devices-page/devices-page.component';
-import { DeviceMappingPageComponent } from './radar-device/pages/device-mapping-page/device-mapping-page.component';
-import { NewDevicePageComponent } from './radar-device/pages/new-device-page/new-device-page.component';
-import { SettingsPageComponent } from './homepage/pages/settings-page/settings-page.component';
+import { DevicePageComponent } from './pages/device-page/device-page.component';
+import { DevicesPageComponent } from './pages/devices-page/devices-page.component';
+import { DeviceMappingPageComponent } from './pages/device-mapping-page/device-mapping-page.component';
+import { NewDevicePageComponent } from './pages/new-device-page/new-device-page.component';
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
 
-import { HomepageComponent } from './homepage/pages/homepage/homepage.component';
-import { ErrorPageNotFoundComponent} from './homepage/pages/error-page-not-found/error-page-not-found.component';
-import { ErrorPageNoServiceComponent } from './homepage/pages/error-page-no-service/error-page-no-service.component';
-import { TracksViewerComponent } from './radar-device/pages/tracks-viewer/tracks-viewer.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ErrorPageNotFoundComponent} from './pages/error-page-not-found/error-page-not-found.component';
+import { ErrorPageNoServiceComponent } from './pages/error-page-no-service/error-page-no-service.component';
+import { TracksViewerComponent } from './pages/tracks-viewer/tracks-viewer.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
--- web_app/src/app/app.module.ts
+++ web_app/src/app/app.module.ts
@@ -5,27 +5,79 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HttpClientModule } from '@angular/common/http'
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatRadioModule} from '@angular/material/radio';
 
 
-import { HomepageModule } from './homepage/homepage.module';
-import { RadarDeviceModule } from './radar-device/radar-device.module';
+import { DevicePageComponent } from './pages/device-page/device-page.component';
+import { DevicesPageComponent } from './pages/devices-page/devices-page.component';
+import { DeviceMappingPageComponent } from './pages/device-mapping-page/device-mapping-page.component';
+import { NewDevicePageComponent } from './pages/new-device-page/new-device-page.component';
+import { SetNetworkDialogComponent } from './components/set-network-dialog/set-network-dialog.component';
+import { EditRadarInfoDialogComponent } from './components/edit-radar-info-dialog/edit-radar-info-dialog.component';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
+import { TracksViewerComponent } from './pages/tracks-viewer/tracks-viewer.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ErrorPageNotFoundComponent } from './pages/error-page-not-found/error-page-not-found.component';
+import { ErrorPageNoServiceComponent } from './pages/error-page-no-service/error-page-no-service.component';
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ErrorPageNotFoundComponent,
+    ErrorPageNoServiceComponent,
+    SettingsPageComponent,
+    DevicesPageComponent,
+    DevicePageComponent,
+    DeviceMappingPageComponent,
+    NewDevicePageComponent,
+    TracksViewerComponent,
+    SetNetworkDialogComponent,
+    EditRadarInfoDialogComponent,
+    ConfirmDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
 
-    HomepageModule,
-    RadarDeviceModule
+    MatToolbarModule,
+    MatButtonModule,    
+    MatTableModule,
+    MatChipsModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatDividerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSidenavModule,
+    MatDialogModule,
+    MatRadioModule,
+
+    HttpClientModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
